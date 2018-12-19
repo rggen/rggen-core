11d63f283a8432095021118d39513f501777d8f1
taichi <taichi730@gmail.com>
2018-12-20 02:06:45 +0900

    added OutputBase::Component (refs #3)

--- lib/rggen/core/output_base/component.rb
+++ lib/rggen/core/output_base/component.rb
@@ -2,7 +2,83 @@ module RgGen
   module Core
     module OutputBase
       class Component < Base::Component
+        include Base::HierarchicalAccessors
 
+        attr_reader :configuration
+        attr_reader :source
+
+        def post_initialize(_paren, configuration, source)
+          @configuration = configuration
+          @source = source
+          @need_children = source.need_children?
+          define_hierarchical_accessors
+          define_field_accessors
+        end
+
+        def add_item(item)
+          super
+          define_item_method_accessor(item)
+        end
+
+        def build
+          @items.each { |_, item| item.build }
+          @children.each(&:build)
+        end
+
+        def generate_code(kind, mode, code = nil)
+          code_generators(mode).inject(code) do |c, g|
+            g.call(kind, mode, c)
+          end
+        end
+
+        def write_file(directory = nil)
+          @items.each { |_, item| item.write_file(directory) }
+          @children.each { |component| component.write_file(directory) }
+        end
+
+        private
+
+        def define_field_accessors
+          def_delegators(:@source, *@source.fields)
+        end
+
+        def define_item_method_accessor(item)
+          target = "@items[:#{item.item_name}]"
+          def_delegators(target, *item.exported_methods)
+        end
+
+        def code_generators(mode)
+          [
+            item_code_generator(:pre),
+            *main_code_generators(mode),
+            item_code_generator(:post)
+          ]
+        end
+
+        def main_code_generators(mode)
+          case mode
+          when :top_down
+            [item_code_generator(:main), child_component_code_generator]
+          when :bottom_up
+            [child_component_code_generator, item_code_generator(:main)]
+          end
+        end
+
+        def item_code_generator(phase)
+          lambda do |kind, _mode, code|
+            @items.inject(code) do |c, (_name, item)|
+              item.generate_code(phase, kind, c)
+            end
+          end
+        end
+
+        def child_component_code_generator
+          lambda do |kind, mode, code|
+            @children.inject(code) do |c, component|
+              component.generate_code(kind, mode, c)
+            end
+          end
+        end
       end
     end
   end
---
+++ spec/rggen/core/output_base/component_spec.rb
@@ -0,0 +1,404 @@
+require 'spec_helper'
+
+module RgGen::Core::OutputBase
+  describe Component do
+    let(:configuration) do
+      RgGen::Core::Configuration::Component.new(nil)
+    end
+
+    let(:register_map) do
+      RgGen::Core::RegisterMap::Component.new(nil, configuration)
+    end
+
+    def create_component(parent)
+      component = Component.new(parent, configuration, register_map)
+      parent && parent.add_child(component)
+      component
+    end
+
+    def define_and_create_item(component, item_name, super_class = nil, &body)
+      item = Class.new(super_class || Item, &body).new(component, item_name)
+      component.add_item(item)
+      item
+    end
+
+    it "レジスタマップオブジェクトの各フィールドメソッドを呼び出すことができる" do
+      allow(register_map).to receive(:fields).and_return([:foo, :bar, :baz])
+      expect(register_map).to receive(:foo)
+      expect(register_map).to receive(:bar)
+      expect(register_map).to receive(:baz)
+
+      component = create_component(nil)
+      component.foo
+      component.bar
+      component.baz
+    end
+
+    it "配下のアイテムのItem.exportで指定されたメソッドを呼び出すことができる" do
+      component = create_component(nil)
+
+      foo_item = define_and_create_item(component, :foo) do
+        export :foo_0
+        export :foo_1
+      end
+      expect(foo_item).to receive(:foo_0)
+      expect(foo_item).to receive(:foo_1)
+
+      bar_item = define_and_create_item(component, :bar) do
+        export :bar_0
+      end
+      expect(bar_item).to receive(:bar_0)
+
+      component.foo_0
+      component.foo_1
+      component.bar_0
+    end
+
+    describe "#need_children?" do
+      let(:component) { create_component(nil) }
+
+      context "レジスタマップオブジェクトが子コンポーネントを必要とする場合" do
+        specify "同様に子コンポーネントを必要とする" do
+          allow(register_map).to receive(:need_children?).and_return(true)
+          expect(component.need_children?).to be true
+        end
+      end
+
+      context "レジスタマップオブジェクトが子コンポーネントを必要としない場合" do
+        specify "同様に子コンポーネントを必要としない" do
+          allow(register_map).to receive(:need_children?).and_return(false)
+          expect(component.need_children?).to be false
+        end
+      end
+    end
+
+    describe "#build" do
+      let(:foo_component) do
+        create_component(nil)
+      end
+
+      let(:bar_components) do
+        2.times.map { create_component(foo_component) }
+      end
+
+      let(:baz_components) do
+        bar_components.flat_map do |bar_component|
+          2.times.map { create_component(bar_component) }
+        end
+      end
+
+      let(:foo_items) do
+        [
+          define_and_create_item(foo_component, :foo_0),
+          define_and_create_item(foo_component, :foo_1),
+        ]
+      end
+
+      let(:bar_items) do
+        bar_components.flat_map do |bar_component|
+          [
+            define_and_create_item(bar_component, :bar_0),
+            define_and_create_item(bar_component, :bar_1)
+          ]
+        end
+      end
+
+      let(:baz_items) do
+        baz_components.flat_map do |baz_component|
+          [
+            define_and_create_item(baz_component, :baz_0),
+            define_and_create_item(baz_component, :baz_1)
+          ]
+        end
+      end
+
+      it "配下の全コンポーネント/アイテムの#buildを呼び出して、自身の組み立てを行う" do
+        [*bar_components, *baz_components].each do |component|
+          expect(component).to receive(:build).and_call_original
+        end
+        [*foo_items, *bar_items, *baz_items].each do |item|
+          expect(item).to receive(:build).and_call_original
+        end
+        foo_component.build
+      end
+    end
+
+    describe "#generate_code" do
+      let(:code) { double('code') }
+
+      let(:foo_component) do
+        create_component(nil)
+      end
+
+      let(:bar_components) do
+        2.times.map { create_component(foo_component) }
+      end
+
+      let(:baz_components) do
+        bar_components.flat_map do |bar_component|
+          2.times.map { create_component(bar_component) }
+        end
+      end
+
+      before do
+        allow_any_instance_of(Item).to receive(:create_blank_code).and_return(code)
+      end
+
+      before do
+        fizz_body = proc do
+          main_code(:fizz) { "#{component.object_id}_#{'fizz' * (component.level + 1)}" }
+        end
+        buzz_body = proc do
+          main_code(:buzz) { "#{component.object_id}_#{'buzz' * (component.level + 1)}" }
+        end
+
+        define_and_create_item(foo_component, :fizz, &fizz_body)
+        define_and_create_item(foo_component, :buzz, &buzz_body)
+
+        bar_components.each do |bar_component|
+          define_and_create_item(bar_component, :fizz, &fizz_body)
+          define_and_create_item(bar_component, :buzz, &buzz_body)
+        end
+
+        baz_components.each do |baz_component|
+          define_and_create_item(baz_component, :fizz, &fizz_body)
+          define_and_create_item(baz_component, :buzz, &buzz_body)
+        end
+      end
+
+      context "modeに:top_downを指定した場合" do
+        it "kindで指定したコードを上位コンポーネントから生成する" do
+          [
+            "#{foo_component.object_id}_fizz",
+            "#{bar_components[0].object_id}_fizzfizz",
+            "#{baz_components[0].object_id}_fizzfizzfizz",
+            "#{baz_components[1].object_id}_fizzfizzfizz",
+            "#{bar_components[1].object_id}_fizzfizz",
+            "#{baz_components[2].object_id}_fizzfizzfizz",
+            "#{baz_components[3].object_id}_fizzfizzfizz"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:fizz, :top_down)
+
+          [
+            "#{foo_component.object_id}_buzz",
+            "#{bar_components[0].object_id}_buzzbuzz",
+            "#{baz_components[0].object_id}_buzzbuzzbuzz",
+            "#{baz_components[1].object_id}_buzzbuzzbuzz",
+            "#{bar_components[1].object_id}_buzzbuzz",
+            "#{baz_components[2].object_id}_buzzbuzzbuzz",
+            "#{baz_components[3].object_id}_buzzbuzzbuzz"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:buzz, :top_down, code)
+        end
+      end
+
+      context "modeに:bottom_upを指定した場合" do
+        it "kindで指定したコードを下位コンポーネントから生成する" do
+          [
+            "#{baz_components[0].object_id}_fizzfizzfizz",
+            "#{baz_components[1].object_id}_fizzfizzfizz",
+            "#{bar_components[0].object_id}_fizzfizz",
+            "#{baz_components[2].object_id}_fizzfizzfizz",
+            "#{baz_components[3].object_id}_fizzfizzfizz",
+            "#{bar_components[1].object_id}_fizzfizz",
+            "#{foo_component.object_id}_fizz"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:fizz, :bottom_up)
+
+          [
+            "#{baz_components[0].object_id}_buzzbuzzbuzz",
+            "#{baz_components[1].object_id}_buzzbuzzbuzz",
+            "#{bar_components[0].object_id}_buzzbuzz",
+            "#{baz_components[2].object_id}_buzzbuzzbuzz",
+            "#{baz_components[3].object_id}_buzzbuzzbuzz",
+            "#{bar_components[1].object_id}_buzzbuzz",
+            "#{foo_component.object_id}_buzz"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:buzz, :bottom_up, code)
+        end
+      end
+
+      it "内部で生成したコードオブジェクト、または、与えたコードオブジェクトを返す" do
+        allow(code).to receive(:<<)
+        expect(foo_component.generate_code(:fizz, :top_down)).to eq code
+        expect(foo_component.generate_code(:fizz, :top_down, code)).to eq code
+        expect(foo_component.generate_code(:fizz, :bottom_up)).to eq code
+        expect(foo_component.generate_code(:fizz, :bottom_up, code)).to eq code
+      end
+
+      context "Item.pre_codeで事前コード生成の登録がある場合" do
+        before do
+          pre_fizz_0_body = proc do
+            pre_code(:fizz) { "#{component.object_id}_pre_#{'fizz' * (component.level + 1)}_0" }
+          end
+          pre_fizz_1_body = proc do
+            pre_code(:fizz) { "#{component.object_id}_pre_#{'fizz' * (component.level + 1)}_1" }
+          end
+
+          define_and_create_item(foo_component, :pre_fizz_0, &pre_fizz_0_body)
+          define_and_create_item(foo_component, :pre_fizz_1, &pre_fizz_1_body)
+
+          bar_components.each do |bar_component|
+            define_and_create_item(bar_component, :pre_fizz_0, &pre_fizz_0_body)
+            define_and_create_item(bar_component, :pre_fizz_1, &pre_fizz_1_body)
+          end
+        end
+
+        it "Item.main_codeで登録された主コード生成前に、事前コードを生成する" do
+          [
+            "#{foo_component.object_id}_pre_fizz_0",
+            "#{foo_component.object_id}_pre_fizz_1",
+            "#{foo_component.object_id}_fizz",
+            "#{bar_components[0].object_id}_pre_fizzfizz_0",
+            "#{bar_components[0].object_id}_pre_fizzfizz_1",
+            "#{bar_components[0].object_id}_fizzfizz",
+            "#{baz_components[0].object_id}_fizzfizzfizz",
+            "#{baz_components[1].object_id}_fizzfizzfizz",
+            "#{bar_components[1].object_id}_pre_fizzfizz_0",
+            "#{bar_components[1].object_id}_pre_fizzfizz_1",
+            "#{bar_components[1].object_id}_fizzfizz",
+            "#{baz_components[2].object_id}_fizzfizzfizz",
+            "#{baz_components[3].object_id}_fizzfizzfizz"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:fizz, :top_down, code)
+
+          [
+            "#{foo_component.object_id}_pre_fizz_0",
+            "#{foo_component.object_id}_pre_fizz_1",
+            "#{bar_components[0].object_id}_pre_fizzfizz_0",
+            "#{bar_components[0].object_id}_pre_fizzfizz_1",
+            "#{baz_components[0].object_id}_fizzfizzfizz",
+            "#{baz_components[1].object_id}_fizzfizzfizz",
+            "#{bar_components[0].object_id}_fizzfizz",
+            "#{bar_components[1].object_id}_pre_fizzfizz_0",
+            "#{bar_components[1].object_id}_pre_fizzfizz_1",
+            "#{baz_components[2].object_id}_fizzfizzfizz",
+            "#{baz_components[3].object_id}_fizzfizzfizz",
+            "#{bar_components[1].object_id}_fizzfizz",
+            "#{foo_component.object_id}_fizz"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:fizz, :bottom_up, code)
+        end
+      end
+
+      context "Item.post_codeで事後コード生成の登録がある場合" do
+        before do
+          post_fizz_0_body = proc do
+            post_code(:fizz) { "#{component.object_id}_post_#{'fizz' * (component.level + 1)}_0" }
+          end
+          post_fizz_1_body = proc do
+            post_code(:fizz) { "#{component.object_id}_post_#{'fizz' * (component.level + 1)}_1" }
+          end
+
+          define_and_create_item(foo_component, :post_fizz_0, &post_fizz_0_body)
+          define_and_create_item(foo_component, :post_fizz_1, &post_fizz_1_body)
+
+          bar_components.each do |bar_component|
+            define_and_create_item(bar_component, :post_fizz_0, &post_fizz_0_body)
+            define_and_create_item(bar_component, :post_fizz_1, &post_fizz_1_body)
+          end
+        end
+
+        it "Item.main_codeで登録された主コードの生成後に、事後コードを生成する" do
+          [
+            "#{foo_component.object_id}_fizz",
+            "#{bar_components[0].object_id}_fizzfizz",
+            "#{baz_components[0].object_id}_fizzfizzfizz",
+            "#{baz_components[1].object_id}_fizzfizzfizz",
+            "#{bar_components[0].object_id}_post_fizzfizz_0",
+            "#{bar_components[0].object_id}_post_fizzfizz_1",
+            "#{bar_components[1].object_id}_fizzfizz",
+            "#{baz_components[2].object_id}_fizzfizzfizz",
+            "#{baz_components[3].object_id}_fizzfizzfizz",
+            "#{bar_components[1].object_id}_post_fizzfizz_0",
+            "#{bar_components[1].object_id}_post_fizzfizz_1",
+            "#{foo_component.object_id}_post_fizz_0",
+            "#{foo_component.object_id}_post_fizz_1"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:fizz, :top_down, code)
+
+          [
+            "#{baz_components[0].object_id}_fizzfizzfizz",
+            "#{baz_components[1].object_id}_fizzfizzfizz",
+            "#{bar_components[0].object_id}_fizzfizz",
+            "#{bar_components[0].object_id}_post_fizzfizz_0",
+            "#{bar_components[0].object_id}_post_fizzfizz_1",
+            "#{baz_components[2].object_id}_fizzfizzfizz",
+            "#{baz_components[3].object_id}_fizzfizzfizz",
+            "#{bar_components[1].object_id}_fizzfizz",
+            "#{bar_components[1].object_id}_post_fizzfizz_0",
+            "#{bar_components[1].object_id}_post_fizzfizz_1",
+            "#{foo_component.object_id}_fizz",
+            "#{foo_component.object_id}_post_fizz_0",
+            "#{foo_component.object_id}_post_fizz_1"
+          ].each do |expected_code|
+            expect(code).to receive(:<<).with(expected_code).ordered
+          end
+          foo_component.generate_code(:fizz, :bottom_up, code)
+        end
+      end
+    end
+
+    describe "#write_file" do
+      let(:foo_component) do
+        create_component(nil)
+      end
+
+      let(:bar_components) do
+        2.times.map { create_component(foo_component) }
+      end
+
+      let(:item_base) do
+        Class.new(Item) do
+          def create_blank_file(_path); ''.dup; end
+        end
+      end
+
+      let(:foo_item) do
+        define_and_create_item(foo_component, :foo_item, item_base) do
+          write_file('<%= file_name %>') { |f| f << file_content }
+          def file_name; "#{component.object_id}_foo.txt"; end
+          def file_content; "#{component.object_id} foo !"; end
+        end
+      end
+
+      let(:bar_items) do
+        bar_components.map.with_index do |bar_component, i|
+          define_and_create_item(bar_component, :bar_item, item_base) do
+            write_file('<%= file_name %>') { |f| f << file_content }
+            define_method(:file_name) { "#{component.object_id}_bar_#{i}.txt" }
+            define_method(:file_content) { "#{component.object_id} bar #{i} !" }
+          end
+        end
+      end
+
+      it "配下の全アイテムの#write_fileを呼び出して、ファイルの書き出しを行う" do
+        [foo_item, *bar_items].each do |item|
+          expect(item).to receive(:write_file).and_call_original
+          expect(File).to receive(:binwrite).with(match_string(item.file_name), item.file_content)
+        end
+        foo_component.write_file
+
+        [foo_item, *bar_items].each do |item|
+          expect(item).to receive(:write_file).and_call_original
+          expect(File).to receive(:binwrite).with(match_string("foo/bar/#{item.file_name}"), item.file_content)
+        end
+        foo_component.write_file(['foo', 'bar'])
+      end
+    end
+  end
+end
