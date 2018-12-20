6d24256ce6958f5fe994ebdfb5103e6dd9ca06de
taichi <taichi730@gmail.com>
2018-12-20 14:37:03 +0900

    implemented monkey patch for issue below https://bugs.ruby-lang.org/issues/12478

--- lib/rggen/core/core_extensions/forwardable.rb
+++ lib/rggen/core/core_extensions/forwardable.rb
@@ -24,6 +24,29 @@ module SingleForwardable
     methods.each { |m| def_object_delegator(target, m) }
   end
 
+  if ['2.3.1', '2.3.2', '2.3.3'].include?(RUBY_VERSION)
+    # https://bugs.ruby-lang.org/issues/12478
+    def def_single_delegator(accessor, method, ali = method)
+      accessor = accessor.to_s
+      if method_defined?(accessor) || private_method_defined?(accessor)
+        accessor = "#{accessor}()"
+      end if self.class === Module
+
+      line_no = __LINE__; str = %{
+        def #{ali}(*args, &block)
+          begin
+            #{accessor}.__send__(:#{method}, *args, &block)
+          rescue ::Exception
+            $@.delete_if{|s| ::Forwardable::FILE_REGEXP =~ s} unless ::Forwardable::debug
+            ::Kernel::raise
+          end
+        end
+      }
+
+      instance_eval(str, __FILE__, line_no)
+    end
+  end
+
   alias_method :object_delegator, :def_object_delegator
   alias_method :object_delegators, :def_object_delegators
 end
