40c219cd1c2e4cdc1642df01f5025ff9499008d9
taichi <taichi730@gmail.com>
2019-02-23 23:08:34 +0900

    added match_value matcher

--- lib/rggen/core/spec_helpers.rb
+++ lib/rggen/core/spec_helpers.rb
@@ -1,9 +1,11 @@
-require_relative 'spec_helpers/have_value_matcher.rb'
-require_relative 'spec_helpers/have_property_matcher.rb'
-require_relative 'spec_helpers/match_string_matcher.rb'
+require_relative 'spec_helpers/have_value_matcher'
+require_relative 'spec_helpers/have_property_matcher'
+require_relative 'spec_helpers/match_string_matcher'
+require_relative 'spec_helpers/match_value_matcher'
 
 RSpec.configure do |config|
   config.include RgGen::Core::SpecHelpers::HaveValueMatcher
   config.include RgGen::Core::SpecHelpers::HavePropertyMatcher
   config.include RgGen::Core::SpecHelpers::MatchStringMatcher
+  config.include RgGen::Core::SpecHelpers::MatchValueMatcher
 end
