65509ada5e8e02d3f7068521057ccd5ce19c4ee1
taichi <taichi730@gmail.com>
2019-03-29 12:06:34 +0900

    separated workaround for below issue https://bugs.ruby-lang.org/issues/12478

--- lib/rggen/core/core_extensions/forwardable.rb
+++ lib/rggen/core/core_extensions/forwardable.rb
@@ -7,29 +7,6 @@ module Forwardable
   end
 end
 
-# :nocov:
 if ['2.3.1', '2.3.2', '2.3.3'].include?(RUBY_VERSION)
-  module SingleForwardable
-    # https://bugs.ruby-lang.org/issues/12478
-    def def_single_delegator(accessor, method, ali = method)
-      accessor = accessor.to_s
-      if method_defined?(accessor) || private_method_defined?(accessor)
-        accessor = "#{accessor}()"
-      end if self.class === Module
-
-      line_no = __LINE__; str = %{
-        def #{ali}(*args, &block)
-          begin
-            #{accessor}.__send__(:#{method}, *args, &block)
-          rescue ::Exception
-            $@.delete_if{|s| ::Forwardable::FILE_REGEXP =~ s} unless ::Forwardable::debug
-            ::Kernel::raise
-          end
-        end
-      }
-
-      instance_eval(str, __FILE__, line_no)
-    end
-  end
+  require_relative 'forwardable_workaround'
 end
-# :nocov:
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,9 @@ require 'bundler/setup'
 require 'rggen/devtools/spec_helper'
 
 RSpec.configure do |config|
-  RgGen::Devtools::SpecHelper.setup(config)
+  RgGen::Devtools::SpecHelper.setup(
+    config, coverage_filter: /forwardable_workaround/
+  )
   config.before(:each) do
     allow(FileUtils).to receive(:mkpath)
   end
