5b1911c3675922fb33d33a68131e617c5674adc6
taichi <taichi730@gmail.com>
2019-04-24 18:36:40 +0900

    added before/after build hook support

--- lib/rggen/core/base/component_factory.rb
+++ lib/rggen/core/base/component_factory.rb
@@ -21,8 +21,14 @@ module RgGen
           parent = (child_factory? && args.first) || nil
           sources = preprocess((child_factory? && args[1..-1]) || args)
           create_component(parent, *sources) do |component|
+            call_build_hook(:before, component)
             build_component(parent, component, sources)
+            call_build_hook(:after, component)
           end
+        end
+
+        def build_hook(phase, &hook)
+          (@build_hooks ||= {})[phase] = hook
         end
 
         private
@@ -63,6 +69,12 @@ module RgGen
 
         def create_child(component, *args)
           @child_factory.create(component, *args)
+        end
+
+        def call_build_hook(phase, component)
+          return unless @build_hooks
+          return unless @build_hooks.key?(phase)
+          @build_hooks[phase].call(component)
         end
       end
     end
