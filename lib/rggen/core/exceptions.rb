476dba7459274a09c455fcf2e1730be8ffbd0641
Taichi Ishitani <taichi730@gmail.com>
2020-08-24 23:35:31 +0900

    added verbose_info field

--- lib/rggen/core/exceptions.rb
+++ lib/rggen/core/exceptions.rb
@@ -3,14 +3,16 @@
 module RgGen
   module Core
     class RgGenError < StandardError
-      def initialize(message, location_info = nil)
+      def initialize(message, location_info = nil, verbose_info = nil)
         super(message)
         @error_message = message
         @location_info = location_info
+        @verbose_info = verbose_info
       end
 
       attr_reader :error_message
       attr_reader :location_info
+      attr_reader :verbose_info
 
       def to_s
         location_info && "#{super} -- #{location_info}" || super
