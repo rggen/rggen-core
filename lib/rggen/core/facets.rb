98340abeb5686834f1a3ef6a29a6bad856dd3b37
Taichi Ishitani <taichi730@gmail.com>
2020-12-12 16:44:12 +0900

    removed facets/hash/at

--- lib/rggen/core/facets.rb
+++ lib/rggen/core/facets.rb
@@ -2,7 +2,6 @@
 
 require 'facets/array/merge'
 require 'facets/file/ext'
-require 'facets/hash/at'
 require 'facets/pathname/to_path'
 require 'facets/module/attr_setter'
 require 'facets/module/lastname'
