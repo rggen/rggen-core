cb3b35bdcd2284ba8d74171d7e9288c2bf1dca23
Taichi Ishitani <taichi730@gmail.com>
2022-01-06 14:22:24 +0900

    remove workaround for Ruby 2.5 support (#96)
    (refs: rggen/rggen#91)

--- lib/rggen/core/input_base/yaml_loader.rb
+++ lib/rggen/core/input_base/yaml_loader.rb
@@ -6,22 +6,13 @@ module RgGen
       module YAMLLoader
         private
 
-        if RUBY_VERSION >= '2.6.0'
-          def yaml_safe_load(yaml, file)
-            YAML.safe_load(
-              yaml,
-              permitted_classes: [Symbol], aliases: true, filename: file,
-              symbolize_names: true
-            )
-          end
-        else
-          def yaml_safe_load(yaml, file)
-            YAML.safe_load(yaml, [Symbol], [], true, file, symbolize_names: true)
-          end
-        end
-
         def load_yaml(file)
-          yaml_safe_load(File.binread(file), file)
+          yaml = File.binread(file)
+          YAML.safe_load(
+            yaml,
+            permitted_classes: [Symbol], aliases: true,
+            filename: file, symbolize_names: true
+          )
         end
       end
     end
